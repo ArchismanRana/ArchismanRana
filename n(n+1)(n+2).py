0d6635bc92fd31907e4fc8250d4ebbf9bf7cb876
ArchismanRana <cavendish.rana@gmail.com>
2024-06-04 10:20:37 +0530

    Add files via upload

---
+++ n(n+1)(n+2).py
@@ -0,0 +1,6 @@
+# Q.5] Write a program in Python that stores a single digit in a variable named as n and print a 3 digit number
+# created as:n(n+1)(n+2)
+n = int(input("Enter a single digit number : "))
+n_1 = n + 1
+n_2 = n + 2
+print("The three digit number is : ", n,n_1,n_2, end="")
